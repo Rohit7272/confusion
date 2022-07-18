574c119d582be7f8ddf865911d25966a92f36dca
rohit <rk264944@gmail.com>
2022-07-18 13:16:04 +0530

    add all files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import {Component} from 'react';
 import Menu from './components/menuComponents';
-
+import { Media } from 'reactstrap';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Hello world
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          
-        </a>
-      </header>
+     <Menu/>
+
+     
     </div>
   );
 }
