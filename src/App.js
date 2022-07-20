181aa972134dd51d796fd74621e0f7358072a76f
rohit <rk264944@gmail.com>
2022-07-20 11:50:16 +0530

    create new dishes components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import {Component} from 'react';
 import Menu from './components/menuComponents';
-import { Media } from 'reactstrap';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-function App() {
-  return (
-    <div className="App">
-    <h1 className='h1' > <b> Fresh Food ! </b> </h1>
-    <Menu/>
-     </div>
-    
-  );
+import {DISH} from './shared/dishes';
+
+class App extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+          dishes: DISH
+      }                         
+  }
+
+render() {
+  return(
+    <div>
+
+      <div>
+      <h1 class="h1" ><b>GOOD FOOD</b></h1>
+      </div>
+
+      <div>
+        <Menu dishes={this.state.dishes}/>
+      </div>
+
+    </div>
+  )
+}
 }
 
 export default App;
